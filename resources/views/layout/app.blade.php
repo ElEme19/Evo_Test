448e113f210e35f3efb182125390300a0d735262
ElEme19 <emilio202219489@ixtapaluca.tecnm.mx>
2025-06-27 13:06:54 -0600

    arreglao

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -207,7 +207,7 @@
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
-<<<<<<< HEAD
+
 
         <!-- Scripts base -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
@@ -220,7 +220,4 @@
 
 </body>
 </html>
-=======
-</body>
-</html>
->>>>>>> 5bb6e52558b9cd603ee89836d0922eb0542c061e
+
