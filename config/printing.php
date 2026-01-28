0bc437127a1707f4e75c75e774784ebdd1a9e4e9
Alexander SC <luis202219896@ixtapaluca.tecnm.mx>
2026-01-28 11:22:40 -0600

    YANOMOVER

--- config/printing.php
+++ config/printing.php
@@ -2,7 +2,7 @@
 
 return [
     'printers' => [
-        'TALLER_1' => 'http://192.168.100.108:5000/print',
+        'TALLER_1' => 'http://192.168.100.183:5000/print',
         #'TALLER_2' => 'http://192.168.1.50:5000/print',
     ],
 ];
